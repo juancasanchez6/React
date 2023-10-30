f6c3c3f091b42394dc271939d07fbc63be5369cd
juancasanchez6 <jcsanchezr04@educarex.es>
2023-10-30 12:56:45 +0100

    Update-react

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  let nombre = "Juanca";
+  let auth = false;
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <label htmlFor="nombre">Nombre</label>
+          <input type="text" id="nombre" />
+          <h1>{nombre}</h1>
+          <p>{auth ? "El usuario está creado" : "El usuario no esta creado"}</p>
+          <p>{2+1}</p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
