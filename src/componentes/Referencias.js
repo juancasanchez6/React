106dd4495a3bff1670944cd808f16c2e43323bc2
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-08 08:56:23 +0100

    Update-Referencias

--- src/componentes/Referencias.js
+++ src/componentes/Referencias.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { createRef, useRef } from "react";
 
 export default function Referencias() {
-  const handleToggleMenu = (e) => {
-    const $menu = document.getElementById("menu");
+  //let refMenu = createRef(),
+  let refMenu = useRef(),
+    refMenuBtn = useRef();
 
-    if (e.target.textContent === "Menú") {
-      e.target.textContent = "Cerrar";
-      $menu.style.display = "block";
-    } else {
-      e.target.textContent = "Menú";
-      $menu.style.display = "none";
-    }
+  //console.log(refMenu, refMenuBtn);
+
+  const handleToggleMenu = (e) => {
+    // const $menu = document.getElementById("menu");
+    // if (e.target.textContent === "Menú") {
+    //   e.target.textContent = "Cerrar";
+    //   $menu.style.display = "block";
+    // } else {
+    //   e.target.textContent = "Menú";
+    //   $menu.style.display = "none";
+    // }
+
+    if (refMenuBtn.current.textContent === "Menú") {
+      refMenuBtn.current.textContent = "Cerrar";
+       refMenu.current.style.display = "block";
+     } else {
+      refMenuBtn.current.textContent = "Menú";
+       refMenu.current.style.display = "none";
+     }
+    
   };
 
   return (
     <>
       <h2>Referencias</h2>
-      <button id="menu-btn" onClick={handleToggleMenu}>
+      <button id="menu-btn" ref={refMenuBtn} onClick={handleToggleMenu}>
         Menú
       </button>
-      <nav id="menu" style={{ display: "none" }}>
+      <nav id="menu" ref={refMenu} style={{ display: "none" }}>
         <a href="#">Seccion 1</a>
         <br />
         <a href="#">Seccion 2</a>
